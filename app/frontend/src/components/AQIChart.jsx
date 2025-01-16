16cdff8ad177227a0719d22ac2ee9e47fd0fc1a1
Subash Singh Dhami <subashdhamee@gmail.com>
2025-01-16 23:30:17 +0545

    Live AQL code added

--- app/frontend/src/components/AQIChart.jsx
+++ app/frontend/src/components/AQIChart.jsx
@@ -1,1 +1,23 @@
 //Charts for visualization
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function LiveAQI() {
+  const [aqiData, setAqiData] = useState(null);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:5000/live_aqi")
+      .then(response => setAqiData(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Live AQI Data</h2>
+      <pre>{JSON.stringify(aqiData, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default LiveAQI;
