b10b5f3be4c319dc3516d30b72b4151b8629a18f
Subash Singh Dhami <subashdhamee@gmail.com>
2025-01-16 23:31:09 +0545

    Forecast added in frontend

--- app/frontend/src/components/Forecast.jsx
+++ app/frontend/src/components/Forecast.jsx
@@ -1,2 +1,22 @@
 //Forecast visualization
- 
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Forecast() {
+  const [forecastData, setForecastData] = useState(null);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:5000/forecast")
+      .then(response => setForecastData(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Forecast AQI</h2>
+      <pre>{JSON.stringify(forecastData, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default Forecast;
